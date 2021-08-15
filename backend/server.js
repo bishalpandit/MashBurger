8338c8d1691e128264ecd03aec70700e078af9d8
bishalpandit <bishalpandit17@gmail.com>
2021-08-15 16:16:12 +0530

    created database models

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,28 @@
 const express = require('express');
+const products = require('./data/products');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(p => p._id === req.params.id);
+    res.json(product);
+
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log('Server running'))
