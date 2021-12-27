8c27814befd0b4b20cfdaaada3fa22e6755387f9
bishalpandit <bishalpandit17@gmail.com>
2021-12-27 19:32:38 +0530

    Configuring Users Routes, Controller

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,9 +1,10 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const { generateToken } = require('../utils/generateToken.js')
 
 //@desc auth user and get token
 //@route POST /api/users/login
-
+//@access Public
 const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
@@ -16,7 +17,7 @@ const authUser = asyncHandler(async (req, res) => {
                 name: user.name,
                 email: user.email,
                 isAdmin: user.isAdmin,
-                token: null
+                token: generateToken(user._id)
             }
         )
     }
@@ -26,6 +27,73 @@ const authUser = asyncHandler(async (req, res) => {
     }
 });
 
+
+//@desc create a new user
+//@route POST /api/users/register
+//@access Public
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    else {
+
+        const user = await User.create({
+            name,
+            email,
+            password
+        })
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id)
+            })
+        }
+        else {
+            res.status(400)
+            throw new Error('Couldn\'t create user! Something went wrong!')
+        }
+    }
+});
+
+
+//@desc Get user profile
+//@route GET /api/users/profile
+//@access Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json(
+            {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+                token: generateToken(user._id)
+            }
+        )
+    }
+    else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+});
+
+
 module.exports = {
-    authUser
+    authUser,
+    getUserProfile,
+    registerUser
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { authUser } = require('../controllers/userControllers')
+const { authUser, getUserProfile, registerUser } = require('../controllers/userControllers')
+const {protect} = require('../middleware/authMiddleware.js')
 
 router.route('/login').post(authUser);
-
+router.route('/register').post(registerUser);
+router.route('/profile').get(protect, getUserProfile)
 module.exports = router;
