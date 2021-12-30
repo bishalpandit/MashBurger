93581b8ea95f95e37716d2ed582b3b5c1dd1dab4
bishalpandit <bishalpandit17@gmail.com>
2021-12-30 20:55:48 +0530

    Finishing User Profile and Updation

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,7 +3,6 @@ const User = require("../models/userModel.js");
 const asyncHandler = require("express-async-handler");
 
 const protect = asyncHandler(async (req, res, next) => {
-    let token //= req.headers.authorization.split(' ')[1]
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
 
@@ -11,13 +10,16 @@ const protect = asyncHandler(async (req, res, next) => {
             let token = req.headers.authorization.split(' ')[1]
             const decoded = jwt.verify(token, process.env.JWT_SECRET)
             req.user = await User.findById(decoded.id).select('-password')
+            next()
         } catch (error) {
             console.log(error);
             res.status(401)
             throw new Error('Not authorized no token')
         }
     }
-    next()
+    else {
+        throw new Error("Not authorized no token")
+    }
 })
 
 module.exports = {
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { authUser, getUserProfile, registerUser } = require('../controllers/userControllers')
+const { authUser, getUserProfile, registerUser, updateUserProfile } = require('../controllers/userControllers')
 const {protect} = require('../middleware/authMiddleware.js')
 
 router.route('/login').post(authUser);
 router.route('/register').post(registerUser);
-router.route('/profile').get(protect, getUserProfile)
+router.route('/:id').get(protect, getUserProfile).put(protect, updateUserProfile)
 module.exports = router;
